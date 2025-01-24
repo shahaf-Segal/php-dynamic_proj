d799cf9c0fb708e5dd140cf7a4a7064d11a9bd18
shahaf-Segal <shahaf100@outlook.co.il>
2025-01-24 02:55:28 +0200

    added Index file with css

--- Routes/index_get.php
+++ Routes/index_get.php
@@ -1,5 +1,5 @@
 <?php
-
+define('BASE_URL', '/');
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -7,11 +7,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Home Page</title>
+    <style type="text/css">
+        <?php include STYLES_DIR . '/main.css'; ?>
+    </style>
 </head>
 
 <body>
-
+    <h1>index</h1>
 </body>
 
 </html>
