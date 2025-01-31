9366eb825649608256088420ef1a0f9407088c9d
shahaf-Segal <shahaf100@outlook.co.il>
2025-01-31 20:50:30 +0200

    added a db template

---
+++ bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+const INCLUDES_DIR = __DIR__ . '/includes';
+const ROUTES_DIR = __DIR__ . '/routes';
+const STYLES_DIR = __DIR__ . '/Styles';
+const TEMPLATES_DIR = __DIR__ . '/templates';
+const DB_DIR = __DIR__ . '/db';
+
+require_once INCLUDES_DIR . '/router.php';
+require_once INCLUDES_DIR . '/db.php';
--- public/index.php
+++ public/index.php
@@ -2,12 +2,8 @@
 
 declare(strict_types=1);
 
-const INCLUDES_DIR = __DIR__ . '/../includes';
-const ROUTES_DIR = __DIR__ . '/../routes';
-const STYLES_DIR = __DIR__ . '/../Styles';
-const TEMPLATES_DIR = __DIR__ . '/../templates';
+require_once './bootstrap.php';
 
 session_start();
-require_once INCLUDES_DIR . '/router.php';
 
 dispatch($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
