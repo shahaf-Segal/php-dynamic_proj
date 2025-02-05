d6ffa8004c23ac24a4c9961135861b249b6dcafa
shahaf-Segal <shahaf100@outlook.co.il>
2025-02-05 14:48:32 +0200

    added flash

--- includes/view.php
+++ includes/view.php
@@ -5,6 +5,7 @@ function renderView(string $file): void
 {
     include TEMPLATES_DIR . '/header.php';
     include TEMPLATES_DIR . '/nav.php';
+    include TEMPLATES_DIR . '/flash.php';
     include  $file;
     include TEMPLATES_DIR . '/footer.php';
 }
