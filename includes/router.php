c666aee9a9243de5765aea212cd0d86b0b7a8b63
shahaf-Segal <shahaf100@outlook.co.il>
2025-02-05 14:48:46 +0200

    import string manipulation

--- includes/router.php
+++ includes/router.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 require_once __DIR__ . '/view.php';
 require_once __DIR__ . '/response.php';
+require_once __DIR__ . '/stringManipulation.php';
 
 const ALLOWED_METHODS = ['GET', 'POST'];
 const INDEX_PAGE = 'index';
